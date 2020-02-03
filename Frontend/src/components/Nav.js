8c0a8b567c70a8383219c36debd4fefcf965ec6a
Jay Parmani <jsparmani@gmail.com>
2020-02-03 12:21:41 +0530

    Remove Join Slack From Navbar

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -41,11 +41,7 @@ export default class Nav extends Component {
                             Achievments
                         </Link>
                     </div>
-                    <div>
-                        <Link to="/communityJoin" style={styles.noDec}>
-                            Join Slack!
-                        </Link>
-                    </div>
+
                     {/* <div>
                         <Link to="/blog" style={styles.noDec}>
                             Projects
