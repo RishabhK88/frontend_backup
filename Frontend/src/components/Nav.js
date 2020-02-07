31f89ad0d352d2db363a8aef6def9e38c4863be5
jaskeerat789 <jaskee789@gmail.com>
2020-02-08 01:47:25 +0530

    Loda

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -43,7 +43,7 @@ export default class Nav extends Component {
                             top: "0",
                             background: "white",
                             zIndex: 100,
-                            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" 
                         }}>
                         <NavSubPart />
                     </div>
