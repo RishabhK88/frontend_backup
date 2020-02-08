98397080ac1d349648277c3edd08fe8ccd077361
jaskeerat789 <jaskee789@gmail.com>
2020-02-08 13:24:18 +0530

    animated NavBar

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -1,54 +1,68 @@
 /** @format */
+import React, { useEffect, useState } from "react";
+import { animated, useSpring, interpolate } from "react-spring";
 
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-// import "./Nav.style.js";
-
-import styled from "styled-components";
 import NavSubPart from "./NavSubPart";
 
-export default class Nav extends Component {
-    constructor(props) {
-        super(props);
+function Nav(props) {
+  const [Top, setisTop] = useState(true);
+  const [scroll,setScroll] = useState(0)
+  document.addEventListener("scroll", () => {
+    const isTop = window.scrollY < 50;
+    if (isTop !== Top) {
+      setisTop(isTop);
+    }
+    setScroll(window.scrollY)
+  });
 
-        this.state = {
-            isTop: true
-        };
-    }
-    componentDidMount() {
-        document.addEventListener("scroll", () => {
-            const isTop = window.scrollY < 50;
-            if (isTop !== this.state.isTop) {
-                this.setState({ isTop });
-                console.log("LOL");
-                console.log(this.state.isTop);
-            }
-        });
-    }
-    render() {
-        return (
-            <React.Fragment>
-                {this.state.isTop ? (
-                    <div
-                        style={{
-                            position: "sticky",
-                            top: "0"
-                        }}>
-                        <NavSubPart active={this.props.active} />
-                    </div>
-                ) : (
-                    <div
-                        style={{
-                            position: "sticky",
-                            top: "0",
-                            background: "white",
-                            zIndex: 100,
-                            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" 
-                        }}>
-                        <NavSubPart active={this.props.active} />
-                    </div>
-                )}
-            </React.Fragment>
-        );
-    }
+  const {o} = useSpring({
+      from:{o:0},
+      o:scroll/100<1?scroll/100:1
+  })
+  
+  return (
+    <React.Fragment>
+      {/* {Top ? ( */}
+        <animated.div
+          style={{
+            position: "sticky",
+            top: "0",
+            //boxShadow: "0 0px 0px 0 rgba(0, 0, 0, 0.0)",
+            //background:"transparent",
+            zIndex:100,
+            background:o.interpolate(o=>`rgba(255, 255, 255, ${o}`),
+            boxShadow:o.interpolate(o=>`0 ${o*2}px ${o*12}px 0 rgba(0,0,0,${o*.18})`)
+          }}
+        >
+          <NavSubPart active={props.active} />
+        </animated.div>
+      {/* ) : (
+        <animated.div
+          style={{
+            position: "sticky",
+            top: "0",
+            background: "white",
+            zIndex: 100,
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+          }}
+        >
+          <NavSubPart active={props.active} />
+        </animated.div>
+      )} */}
+      {/* <animated.dev
+
+        style={{
+            position:'sticky',
+            top:0,
+            boxShadow:"0 0px 0px 0 rgba(0, 0, 0, 0.0)",
+            backgroundColor:'white',
+
+        }}
+      >
+          <NavSubPart/>
+      </animated.dev> */}
+    </React.Fragment>
+  );
 }
+export default Nav;
